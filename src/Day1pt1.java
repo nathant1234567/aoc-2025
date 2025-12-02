c796e0698cc3bc94092df575c3cc1ff4e58a16f5
NathanT <nathanthompson256@outlook.com>
2025-12-02 00:29:25 +0000

    day 1 done woo in java as well cause i like to torture myself

---
+++ src/Day1pt1.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Day1pt1 {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\user\\OneDrive - University of Kent\\102. Coding things\\AOC2026\\day1-test.txt");
+        Scanner scanner = new Scanner(file);
+
+        int current = 50;
+        int password = 0;
+
+        while (scanner.hasNextLine()) {
+            String currentPos = scanner.nextLine();
+
+            if (currentPos.charAt(0) == 'R') {
+                System.out.println(currentPos + " from " + current);
+                current += Integer.parseInt(currentPos.substring(1));
+
+                current = current % 100;
+
+                if (current == 0) {
+                    password++;
+                }
+
+            } else {
+                System.out.println(currentPos + " from " + current);
+                current -= Integer.parseInt(currentPos.substring(1));
+
+                current = current % 100;
+                if (current < 0) {
+                    current += 100;
+                }
+
+                if (current == 0) {
+                    password++;
+                }
+            }
+        }
+
+        System.out.println(password);
+    }
+}
---
+++ src/Day1pt2.java
@@ -0,0 +1,37 @@
+// a bit more optimisation here :)
+
+import java.io.File;
+import java.util.Scanner;
+
+public class Day1pt2 {
+
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\user\\OneDrive - University of Kent\\102. Coding things\\AOC2026\\day1-test.txt");
+        Scanner scanner = new Scanner(file);
+
+        int current = 50;
+        long password = 0;
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            char dir = line.charAt(0);
+            int amount = Integer.parseInt(line.substring(1));
+
+            if (dir == 'R') {
+                password += (current + amount) / 100;
+
+                current = (current + amount) % 100;
+
+            } else {
+                int forward = (100 - current) % 100;
+
+                password += (forward + amount) / 100;
+
+                current = (current - amount) % 100;
+                if (current < 0) current += 100;
+            }
+        }
+
+        System.out.println(password);
+    }
+}
