b3fc8c1259725d5446bc42c8fb65c64fc2ef5ff2
NathanT <nathanthompson256@outlook.com>
2025-12-03 12:40:36 +0000

    day 2 pt1

---
+++ src/Day2pt1.java
@@ -0,0 +1,40 @@
+import java.io.File;
+import java.util.Scanner;
+
+public class Day2pt1 {
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\user\\OneDrive - University of Kent\\102. Coding things\\AOC2026\\day2-test.txt");
+        Scanner scanner = new Scanner(file);
+        if (!scanner.hasNextLine()) {
+            System.out.println(0);
+            return;
+        }
+        String line = scanner.nextLine().trim();
+
+        String[] rangeTok = line.split(",");
+        long sum = 0L;
+
+        for (String token : rangeTok) {
+            token = token.trim();
+            String[] parts = token.split("-");
+
+            long start = Long.parseLong(parts[0]);
+            long end = Long.parseLong(parts[1]);
+
+            for (long i = start; i < end; i++) {
+                if (invalidId(i)) {
+                    sum += i;
+                }
+            }
+        }
+        System.out.println(sum);
+    }
+
+    private static boolean invalidId(long id) {
+        String s = Long.toString(id);
+        int len = s.length();
+        if (len % 2 != 0) return false;
+        int half = len/2;
+        return s.substring(0, half).equals(s.substring(half));
+    }
+}
